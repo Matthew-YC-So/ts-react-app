d11d7cb87048d4783a8fb5a90fb3c34bf92e8de9
Matthew So <matthew.so@software-force.com>
2021-10-03 21:40:58 +0800

    useState

---
+++ src/Members.ts
@@ -0,0 +1,19 @@
+import { Profile } from './AppTypes'
+
+export const MEMBERS: Profile[] = [
+    {
+        id: 10,
+        firstName: 'Matthew',
+        lastName: 'So',
+        bio: 'A C# developer originated from Hong Kong',
+        phone: '0901320973'
+    },
+    {
+        id: 11,
+        firstName: 'Maggie',
+        lastName: 'Liu',
+        bio: 'A traveller',
+        phone: 'XXX-YYY-ZZZZ'
+    },
+
+];
--- src/MyComp.tsx
+++ src/MyComp.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Table, Button } from 'react-bootstrap';
 import './MyComp.css';
-import MyForm from './profileForm'
-import { Profile } from './AppTypes'
+import MyForm from './profileForm';
+import { Profile } from './AppTypes';
+import { MEMBERS } from './Members';
 
 
-interface record { id: number; name: string }
-
-function MyRow(props: { row: record }) {
-    return (<tr>
-        <td>{props.row.id}</td><td>{props.row.name}</td>
+function MyRow(props: { row: Profile, onClick: any, isActive:boolean }) {
+    return (<tr className={props.isActive?"table-active":""}  >
+        <td><Button variant="link" className="w-100" onClick={() => props.onClick(props.row.id)}>{props.row.id}</Button></td><td>{props.row.firstName},{props.row.lastName}</td><td>{props.row.bio}</td><td>{props.row.phone}</td>
     </tr>);
 }
 
-function MyTable(props: { rows: record[] }) {
+function MyTable(props: { rows: Profile[], onSelected: any, activeRow:number }) {
 
     return (
-        <table>
-            <thead><tr><th>ID</th><th>Name</th></tr></thead>
+        <Table bordered responsive size="sm"  >
+            <thead><tr><th>ID</th><th>Name</th><th>BIO</th><th>Phone</th></tr></thead>
             <tbody>
-                {props.rows.map(x => <MyRow row={x} key={x.id} />)}
+                {props.rows.map((x, index) => <MyRow row={x} key={x.id} onClick={(id: number) => props.onSelected(id)} isActive={ index === props.activeRow }  />)}
             </tbody>
-        </table>
+        </Table>
     );
 
 }
 
 export default function MyComp() {
 
-    var data = [{ id: 1, name: 'Matthew' }, { id: 2, name: 'Maggie' }];
-    var profile: Profile = { firstName: 'Matthew', lastName: 'SO', bio: '', phone: '0901320973' };
+    const members = MEMBERS;
+    const emptyProfile = { id: 0, firstName: '', lastName: '', bio: '', phone: '' };
 
-    const onProfileChanged = (values: Profile) => {
-        window.alert(JSON.stringify(values));
+    function getProfile(id: number) {
+        if (id) {
+            let mbr = members.find(m => m.id === id)
+            if (mbr !== undefined)
+                setProfile(mbr);
+            else
+                setProfile(emptyProfile);
+        }
+    }
+
+    const currentProfile = members.length > 0 ? members[0] : emptyProfile;
+
+    const [profile, setProfile] = useState(currentProfile);
+
+    const onProfileChanged = (profile: Profile) => {
+        // window.alert(JSON.stringify(profile));
+        const nonModProfile = members.find(m => m.id === profile.id) ;
+        const updatedProfile = { ...nonModProfile, ...profile };
+        const keyIndex = members.findIndex(c => c.id === nonModProfile?.id);
+        members[keyIndex] = updatedProfile;
+        setProfile(updatedProfile);
     };
 
+    let currentRowIndex = members.findIndex(c=>c.id === profile.id);
 
     return (
         <div>
-            <MyTable rows={data} />
+            <MyTable rows={members} onSelected={(id: number) => getProfile(id)} activeRow={currentRowIndex}  />
             <MyForm profile={profile} onChange={onProfileChanged} />
         </div>
     );
